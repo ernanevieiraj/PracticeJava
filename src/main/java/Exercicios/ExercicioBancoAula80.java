9c9efaa35d53fc05c5efd8b32eca3865062f8357
Ernane Vieira <ernanevieiraj@hotmail.com>
2020-07-25 16:42:19 -0300

    Adicionando exercicios novas aulas

--- src/main/java/Exercicios/ExercicioBancoAula80.java
+++ src/main/java/Exercicios/ExercicioBancoAula80.java
@@ -21,6 +21,17 @@ public class ExercicioBancoAula80 {
         this.name = name;
     }
 
+    public ExercicioBancoAula80( int account, String name ) {
+        this.account = account;
+        this.name = name;
+    }
+
+    public ExercicioBancoAula80( int account, String name, double initialDeposit ) {
+        this.account = account;
+        this.name = name;
+        addBalance(initialDeposit);
+    }
+
     public double getBalance() {
         return balance;
     }
@@ -40,4 +51,28 @@ public class ExercicioBancoAula80 {
                 + String.format("%.2f", balance);
     }
 
+//    ExercicioBancoAula80 banco;
+//        System.out.println("Enter account number: ");
+//    int account = Integer.parseInt(scanner.nextLine());
+//        System.out.println("Enter account holder: ");
+//    String name = scanner.nextLine();
+//        System.out.println("Enter initial deposit value (y/n)?");
+//    char response = scanner.next().charAt(0);
+//        if (response == 'y') {
+//        System.out.println("Enter initial deposit value: ");
+//        double initalDeposit = scanner.nextDouble();
+//        banco = new ExercicioBancoAula80(account,name,initalDeposit);
+//    }
+//        else {
+//        banco = new ExercicioBancoAula80(account,name);
+//    }
+//        System.out.println("Account data: " + banco);
+//        System.out.println("Enter a deposit value: ");
+//    double depositValue = scanner.nextDouble();
+//        banco.addBalance(depositValue);
+//        System.out.println("Updated account data: " + banco);
+//        System.out.println("Enter a withdraw value: ");
+//    double withdrawValue = scanner.nextDouble();
+//        banco.removeBalance(withdrawValue);
+//        System.out.println("Updated account data: " + banco);
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,23 +6,7 @@ public class Main {
 
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        ExercicioBancoAula80 banco = new ExercicioBancoAula80();
-        System.out.println("Enter account number: ");
-        banco.setAccount(Integer.parseInt(scanner.nextLine()));
-        System.out.println("Enter account holder: ");
-        banco.setName(scanner.nextLine());
-        System.out.println("Enter initial deposit value (y/n)?");
-        if (scanner.next().equals("y")) {
-            System.out.println("Enter initial deposit value: ");
-            banco.addBalance(scanner.nextDouble());
-        }
-        System.out.println("Account data: " + banco);
-        System.out.println("Enter a deposit value: ");
-        banco.addBalance(scanner.nextDouble());
-        System.out.println("Updated account data: " + banco);
-        System.out.println("Enter a withdraw value: ");
-        banco.removeBalance(scanner.nextDouble());
-        System.out.println("Updated account data: " + banco);
+
         scanner.close();
 
     }
