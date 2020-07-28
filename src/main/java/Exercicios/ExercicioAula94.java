abad1ce752ed0e1d573542c979105f9087b2c8a9
Ernane Vieira <ernanevieiraj@hotmail.com>
2020-07-27 23:46:10 -0300

    Adicionando exercicios novas aulas

--- src/main/java/Exercicios/ExercicioAula94.java
+++ src/main/java/Exercicios/ExercicioAula94.java
@@ -1,5 +1,8 @@
 package Exercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExercicioAula94 {
 
     private Integer id;
@@ -49,4 +52,31 @@ public class ExercicioAula94 {
                 + String.format("%.2f",salary);
 
     }
+//    List<ExercicioAula94> employee = new ArrayList<>();
+//
+//        System.out.print("How many employees will be registered? ");
+//    int quantityEmployees = Integer.parseInt(scanner.nextLine());
+//        for(int i = 0; i < quantityEmployees; i++){
+//        System.out.print("Id: ");
+//        Integer id = Integer.parseInt(scanner.nextLine());
+//        System.out.print("Name: ");
+//        String name = scanner.nextLine();
+//        System.out.print("Salary: ");
+//        Double salary = Double.parseDouble(scanner.nextLine());
+//        employee.add(new ExercicioAula94(id, name, salary));
+//    }
+//        System.out.print("Enter the employee id that will have salary increase: ");
+//    int id =scanner.nextInt();
+//    ExercicioAula94 updateEmp = employee.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+//        if (updateEmp == null){
+//        System.out.print("This id does not exi3st");
+//    }else {
+//        System.out.print("Enter the porcentage: ");
+//        Double percentage = scanner.nextDouble();
+//        updateEmp.salaryIncrease(percentage);
+//    }
+//
+//        for(ExercicioAula94 emplo : employee){
+//        System.out.println(emplo.toString());
+//    }
 }
---
+++ src/main/java/Exercicios/aulaMatriz.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+public class aulaMatriz {
+//    int tamanhoMatriz = scanner.nextInt();
+//    int[][] mat = new int[tamanhoMatriz][tamanhoMatriz];
+//        for (int i=0; i<tamanhoMatriz; i++){
+//        for (int j=0; j<tamanhoMatriz; j++){
+//            mat[i][j] = scanner.nextInt();
+//        }
+//    }
+//        System.out.print("Main diagonal: ");
+//    int negativeNumbers = 0;
+//        for (int i=0; i<tamanhoMatriz; i++){
+//        for (int j=0; j<tamanhoMatriz; j++){
+//            if (mat[i][j] < 0){
+//                negativeNumbers+= 1;
+//            }
+//            if (i == j){
+//                System.out.println(mat[i][j]);
+//            }
+//        }
+//    }
+//        System.out.println("Negative numbers = " + negativeNumbers);
+//
+
+}
